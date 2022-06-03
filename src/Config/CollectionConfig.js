a5d90d41da507248dc5916e82091dfc55494e546
James Iacabucci <iacabucci@outlook.com>
2022-06-03 00:05:01 -0700

    new contract

--- src/Config/CollectionConfig.js
+++ src/Config/CollectionConfig.js
@@ -1,6 +1,6 @@
 const CollectionConfig = {
   contractName: 'SheBloomsCollection',
-  contractAddress: "0x2D59A6bB2E875e03eCD85Efbf2BdF84db39bfa0d",
+  contractAddress: "0x04580f7a1B1A429df3f58719D3D656e4CDB29d49",
   //contractAddress: "0xFFbBbCCC990bf3205467F8Fd3f656aa72F86817a",
   stakingAddress: "0x00D393033DC6a029e2A4952fBd6960A712E02C2f",
   tokenAddress: "0x60A0E01A1AFc273533aE03F8693dce52F8cD0C1b"
